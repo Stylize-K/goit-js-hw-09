454c273cd8161e0d5533647a884d508a111a1c71
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-05-20 04:01:20 +0300

    Task 2 - complete

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,11 +4,8 @@ import Notiflix from 'notiflix';
 
 const btnStart = document.querySelector('button[data-start]');
 
+//Блокуємо кнопку "Start" одразу після зарантаження сторінки
 btnStart.disabled = true;
-
-let delta = null;
-let targetTime = null;
-let currentTime = null;
 
 // Об'єкт налаштувань для функції бібліотеки "flatpickr"
 const options = {
@@ -17,16 +14,12 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    targetTime = selectedDates[0].getTime();
-    currentTime = Date.now();
-    delta = targetTime - currentTime;
-    // console.log(currentTime);
-    // console.log(targetTime);
-    // console.log(delta);
-    if (delta < 0) {
+    console.log(selectedDates[0]);
+    if (Date.now() > selectedDates[0]) {
       Notiflix.Notify.failure('Please, choose a date in the future');
     } else {
       btnStart.disabled = false;
+      timer.setTargetDate(selectedDates[0]);
     }
   },
 };
@@ -34,46 +27,55 @@ const options = {
 // Ініціалізація бібліотеки "flatpickr" на елементі input
 const fp = flatpickr('#datetime-picker', options);
 
-btnStart.addEventListener('click', onBtnStartClick);
-
-// Функція, що додає 0, якщо в числі менше двох символів
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-
 //Створення класу countDownTimer
 class countDownTimer {
-  constructor({ selector, targetDate }) {
-    this.targetDate = targetDate;
+  constructor({ selector }) {
+    this.targetDate;
     this.daysSpan = document.querySelector(`${selector} [data-days]`);
     this.hoursSpan = document.querySelector(`${selector} [data-hours]`);
     this.minutesSpan = document.querySelector(`${selector} [data-minutes]`);
     this.secondsSpan = document.querySelector(`${selector} [data-seconds]`);
   }
+  //Метод класу, що приймає обрану дату з календаря та додає її у властивість targetDate
+  setTargetDate(targetDate) {
+    this.targetDate = targetDate;
+  }
 
+  //Метод класу, що генерує розмітку таймера
   updateMarkup() {
-    setInterval(() => {
-      currentTime = Date.now();
-      delta = this.targetDate - currentTime;
+    const intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const delta = this.targetDate - currentTime;
       const { days, hours, minutes, seconds } = this.convertMs(delta);
-      console.log(delta);
       this.daysSpan.textContent = days;
       this.hoursSpan.textContent = hours;
       this.minutesSpan.textContent = minutes;
       this.secondsSpan.textContent = seconds;
+
+      if (delta < 1000) {
+        clearInterval(intervalId);
+      }
     }, 1000);
   }
 
+  // Метод класу, що додає 0, якщо в числі менше двох символів (для форматування часу)
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  //Метод, що приймає значення в мілісекундах і повертає days, hours, minutes, seconds (від індуса :) )
   convertMs(ms) {
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
 
-    const days = addLeadingZero(Math.floor(ms / day));
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    const seconds = addLeadingZero(
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    const seconds = this.addLeadingZero(
       Math.floor((((ms % day) % hour) % minute) / second)
     );
 
@@ -82,13 +84,9 @@ class countDownTimer {
 }
 
 //Ініціалізація таймера (створення еземплеру класу countDownTimer)
-const timer1 = new countDownTimer({
+const timer = new countDownTimer({
   selector: '.timer',
-  // targetDate: new Date('Jul 1, 2023'),
-  targetDate: targetTime,
 });
 
-//Функція запуску таймера по кліку кнопки "Start"
-function onBtnStartClick() {
-  timer1.updateMarkup();
-}
+//Вішаємо слухача на клік кнопки "Start" та передаємо в колбек метод таймера (обов'язкого прив'язуємо контекст через метод bind)
+btnStart.addEventListener('click', timer.updateMarkup.bind(timer));
